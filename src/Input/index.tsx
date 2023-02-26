1ae95b818a80edcb4ad044747c65325b639f13e5
Charlie Long <31471962+CharlieLoong@users.noreply.github.com>
2023-02-26 18:49:05 +0800

    feat: add forwardRef, change style

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,11 +1,23 @@
-import React, { useRef, FC, useState } from 'react';
 import classNames from 'classnames';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 
-import './style.scss'
+import './style.scss';
 
-type InputSize = 'lg' | 'md' | 'sm';
+type InputHeight = 'lg' | 'md' | 'sm';
 
-interface InputProps {
+export interface InputRef {
+  focus: () => void;
+  blur: () => void;
+  setSelectionRange: (
+    start: number,
+    end: number,
+    direction?: 'forward' | 'backward' | 'none',
+  ) => void;
+  select: () => void;
+  input: HTMLInputElement | null;
+}
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   prefix?: string;
   suffix?: string;
   className?: string;
@@ -13,88 +25,96 @@ interface InputProps {
   disabled?: boolean;
   id?: string;
   maxLength?: number;
-  size?: InputSize;
+  height?: InputHeight;
   defaultValue?: string;
   value?: string;
   onChange?: () => void;
   required?: boolean;
   type?: string;
+  label?: string;
 }
 
-const Input: FC<InputProps> = ({
-  prefix = '',
-  suffix = '',
-  className = '',
-  placeholder = 'enter something',
-  disabled = false,
-  id,
-  maxLength,
-  size = 'md',
-  value,
-  defaultValue = '',
-  onChange,
-  required = false,
-  type = 'text'
-}: InputProps) => {
-  const inputRef = useRef(null);
-  const [inputContent, setInputContent] = useState(
-    `${defaultValue}`
-  );
-  
+const Input = forwardRef<InputRef, InputProps>((props, ref) => {
+  const {
+    prefix = '',
+    suffix = '',
+    className = '',
+    placeholder = 'enter something',
+    disabled = false,
+    id,
+    maxLength,
+    height = 'md',
+    defaultValue = '',
+    onChange,
+    required = false,
+    type = 'text',
+    label = '',
+    ...restProps
+  }: InputProps = props;
+  // const [inputContent, setInputContent] = useState(`${defaultValue}`);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    focus,
+    blur: () => {
+      inputRef.current?.blur();
+    },
+    setSelectionRange: (
+      start: number,
+      end: number,
+      direction?: 'forward' | 'backward' | 'none',
+    ) => {
+      inputRef.current?.setSelectionRange(start, end, direction);
+    },
+    select: () => {
+      inputRef.current?.select();
+    },
+    input: inputRef.current,
+  }));
+
   const heightOfSize = {
-    'sm': '24px',
-    'md': '32px',
-    'lg': '40px',
-  }
+    sm: '24px',
+    md: '32px',
+    lg: '40px',
+  };
 
   const classes = {
-    container: classNames(
-      `h-[${heightOfSize[size]}]`,
-      'container'
-    ),
+    container: classNames(`h-[${heightOfSize[height]}]`, 'container'),
     input: classNames(
       { 'rounded-r-md': prefix.length === 0 },
       { 'rounded-l-md': suffix.length === 0 },
       { 'input-disabled': disabled },
       'input',
-      className
+      className,
     ),
   };
 
   return (
     <>
-      <span className={classes.container}>
-        {prefix && (
-          <span className="prefix">
-            {prefix}
-          </span>
-        )}
-        <input
-          className={classes.input}
-          type={type}
-          id={id}
-          name="name"
-          required={required}
-          placeholder={placeholder}
-          minLength={4}
-          maxLength={maxLength}
-          size={10}
-          disabled={disabled}
-          onChange={(e) => {
-            setInputContent(`${prefix}${e.target.value}${suffix}`);
-          }}
-          defaultValue={defaultValue}
-          ref={inputRef}
-        />
-        {suffix && (
-          <span className="suffix">
-            {suffix}
-          </span>
-        )}
-      </span>
-      <p>{inputContent}</p>
+      <label>
+        {label}
+        <span className={classes.container}>
+          {prefix && <span className="prefix">{prefix}</span>}
+          <input
+            className={classes.input}
+            type={type}
+            id={id}
+            name="name"
+            required={required}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            disabled={disabled}
+            onChange={onChange}
+            {...restProps}
+            defaultValue={defaultValue}
+            ref={inputRef}
+          />
+          {suffix && <span className="suffix">{suffix}</span>}
+        </span>
+      </label>
+      {/* <p>{inputContent}</p> */}
     </>
   );
-};
+});
 
 export default Input;
